2caa354b1db45c84992cb65a310cc5b0d56980ee
NickGBR <gubarenko.nikita27@gmail.com>
2021-03-03 10:09:21 +0300

    Добавлен mock для deposit сервиса

--- src/main/java/ru/interns/mock/demo/deposit/controller/DepositController.java
+++ src/main/java/ru/interns/mock/demo/deposit/controller/DepositController.java
@@ -1,12 +1,10 @@
 package ru.interns.mock.demo.deposit.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.interns.mock.demo.deposit.dto.OpenDepositRequestDTO;
@@ -19,7 +17,6 @@ import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/deposit")
