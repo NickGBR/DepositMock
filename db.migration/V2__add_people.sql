23b777e70ca90b3c96ebc074e802b418dd9ccc64
Pavel Trofimov <r-half@yandex.ru>
2021-03-04 19:22:56 +0300

    Add migration

---
+++ db.migration/V2__add_people.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.terrorists (id, address, date_of_birthday, middle_name, name, passport_number, status, surname)
+VALUES (1, '5004100033300400001', '1994-02-12 03:00:00.000000', 'Мельникова', 'Ксения', 2348987558, true, 'Витальевна');
+
+INSERT INTO public.terrorists (id, address, date_of_birthday, middle_name, name, passport_number, status, surname)
+VALUES (2, '7100600100000720001', '2000-02-02 03:00:00.000000', 'Иванова', 'София', 4392888932, false, 'Ивановна');
+
+INSERT INTO public.terrorists (id, address, date_of_birthday, middle_name, name, passport_number, status, surname)
+VALUES (3, '3100000300002940001', '2001-05-04 04:00:00.000000', 'Буракшаева', 'Юлия', 3492866943, false, 'Сергеевна');
+
+INSERT INTO public.terrorists (id, address, date_of_birthday, middle_name, name, passport_number, status, surname)
+VALUES (4, '6100300004200720001', '2002-06-04 04:00:00.000000', 'Фурсова', 'Елизавета', 4396033603, false,
+        'Владимировна');
+
+INSERT INTO public.terrorists (id, address, date_of_birthday, middle_name, name, passport_number, status, surname)
+VALUES (5, '2300000200005670001', '1995-02-24 03:00:00.000000', 'Сапсай', 'Иван', 4396299602, false, 'Алексеевич');
